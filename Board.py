640b6c923af8cb492c6d50173151b37dd9df18c0
kevinhawryluk <kevin.hawryluk@gmail.com>
2019-11-17 16:47:22 -0500

    Changed game to connect four instead of tic-tac-toe

--- Board.py
+++ Board.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 Carsten Friedrich (Carsten.Friedrich@gmail.com). All rights reserved
+# Based on Tic-tac-toe game: https://github.com/fcarsten/tic-tac-toe
 #
 
 import numpy as np
@@ -11,31 +11,32 @@ class GameResult(Enum):
     Enum to encode different states of the game. A game can be in progress (NOT_FINISHED), lost, won, or draw
     """
     NOT_FINISHED = 0
-    NAUGHT_WIN = 1
-    CROSS_WIN = 2
+    BLACK_WIN = 1
+    RED_WIN = 2
     DRAW = 3
 
 
 #
-# Values to encode the current content of a field on the board. A field can be empty, contain a naught, or
-# contain a cross
+# Values to encode the current content of a field on the board. A field can be empty, contain a red chip, or
+# contain a black chip
 #
 EMPTY = 0  # type: int
-NAUGHT = 1  # type: int
-CROSS = 2  # type: int
+BLACK = 1  # type: int
+RED = 2  # type: int
 
 #
 # Define the length and width of the board. Has to be 3 at the moment, or some parts of the code will break. Also,
 # the game mechanics kind of require this dimension unless other rules are changed as well. Encoding as a variable
 # to make the code more readable
 #
-BOARD_DIM = 3  # type: int
-BOARD_SIZE = BOARD_DIM * BOARD_DIM  # type: int
+BOARD_HEIGHT = 6  # type: int
+BOARD_WIDTH = 7  # type: int
+BOARD_SIZE = BOARD_HEIGHT * BOARD_WIDTH  # type: int
 
 
 class Board:
     """
-    The class to encode a tic-tac-toe board, including its current state of pieces.
+    The class to encode a connect four board, including its current state of pieces.
     Also contains various utility methods.
     """
 
@@ -43,11 +44,11 @@ class Board:
     # We will use these starting positions and directions when checking if a move resulted in the game being
     # won by one of the sides.
     #
-    WIN_CHECK_DIRS = {0: [(1, 1), (1, 0), (0, 1)],
-                      1: [(1, 0)],
-                      2: [(1, 0), (1, -1)],
-                      3: [(0, 1)],
-                      6: [(0, 1)]}
+    WIN_CHECK_DIRS = [[(0, -1), (0, 1)],   # Horizontal
+                      [(-1, -1), (1, 1)],  # Diagonal bottom left to top right
+                      [(1, -1), (-1, 1)]   # Diagonal top left to bottom right
+                      [(-1, 0)]            # Vertical
+                      ]
 
     def hash_value(self) -> int:
         """
@@ -71,11 +72,11 @@ class Board:
         if side == EMPTY:
             raise ValueError("EMPTY has no 'other side'")
 
-        if side == CROSS:
-            return NAUGHT
+        if side == RED:
+            return BLACK
 
-        if side == NAUGHT:
-            return CROSS
+        if side == BLACK:
+            return RED
 
         raise ValueError("{} is not a valid side".format(side))
 
@@ -97,7 +98,7 @@ class Board:
         :param coord: A board position in 2D coordinates
         :return: The same board position in 1D coordinates
         """
-        return coord[0] * BOARD_DIM + coord[1]
+        return coord[0] * BOARD_HEIGHT + coord[1]
 
     def pos_to_coord(self, pos: int) -> (int, int):
         """
@@ -106,7 +107,7 @@ class Board:
         :param pos: A board position in 1D coordinates
         :return: The same board position in 2D coordinates
         """
-        return pos // BOARD_DIM, pos % BOARD_DIM
+        return pos // BOARD_HEIGHT, pos % BOARD_WIDTH
 
     def reset(self):
         """
@@ -127,7 +128,7 @@ class Board:
         :return: A random empty spot on the board in 1D coordinates
         """
         index = np.random.randint(self.num_empty())
-        for i in range(9):
+        for i in range(BOARD_SIZE):
             if self.state[i] == EMPTY:
                 if index == 0:
                     return i
@@ -136,11 +137,12 @@ class Board:
 
     def is_legal(self, pos: int) -> bool:
         """
-        Tests whether a board position can be played, i.e. is currently empty
+        Tests whether a board position can be played. The spot has to be the first empty position in a column.
         :param pos: The board position in 1D that is to be checked
         :return: Whether the position can be played
         """
-        return (0 <= pos < BOARD_SIZE) and (self.state[pos] == EMPTY)
+        return (0 <= pos < BOARD_SIZE) and (self.state[pos] == EMPTY) \
+               and (pos < BOARD_WIDTH or self.state[pos - BOARD_WIDTH] != EMPTY)
 
     def move(self, position: int, side: int) -> (np.ndarray, GameResult, bool):
         """
@@ -149,7 +151,7 @@ class Board:
         returns the new state of the board, the game result after this move, and whether this move has finished the game
 
         :param position: The position where we want to put a piece
-        :param side: What piece we want to play (NAUGHT, or CROSS)
+        :param side: What piece we want to play (RED, or BLACK)
         :return: The game state after the move, The game result after the move, Whether the move finished the game
         """
         if self.state[position] != EMPTY:
@@ -159,14 +161,15 @@ class Board:
         self.state[position] = side
 
         if self.check_win():
-            return self.state, GameResult.CROSS_WIN if side == CROSS else GameResult.NAUGHT_WIN, True
+            return self.state, GameResult.RED_WIN if side == RED else GameResult.BLACK_WIN, True
 
         if self.num_empty() == 0:
             return self.state, GameResult.DRAW, True
 
         return self.state, GameResult.NOT_FINISHED, False
 
-    def apply_dir(self, pos: int, direction: (int, int)) -> int:
+
+    def apply_dir(self, pos: int, direction: (int, int)) -> bool:
         """
         Applies 2D direction dir to 1D position pos.
         Returns the resulting 1D position, or -1 if the resulting position would not be a valid board position.
@@ -175,16 +178,37 @@ class Board:
         :param direction: The direction to apply in 2D
         :return: The resulting 1D position, or -1 if the resulting position would not be a valid board position.
         """
-        row = pos // 3
-        col = pos % 3
-        row += direction[0]
-        if row < 0 or row > 2:
+        row = pos // BOARD_WIDTH
+        col = pos % BOARD_WIDTH
+        row += direction[0] * BOARD_WIDTH
+        if row < 0 or row > BOARD_HEIGHT:
             return -1
-        col += direction[1]
-        if col < 0 or col > 2:
+        col += direction[1] * BOARD_HEIGHT
+        if col < 0 or col > BOARD_WIDTH:
             return -1
 
-        return row * 3 + col
+        return row * BOARD_WIDTH + col
+
+
+    def count_in_direction(self, pos: int, direction: (int, int)) -> int:
+        '''
+        Counts the number of chips that are the same color in a row.
+        :param pos: Position of the chip just dropped.
+        :param direction: Direction from the chip to check.
+        :return Total count of chips in a given direction that are the same color as the starting chip:
+        '''
+        c = self.state[pos]
+        count = 0
+        if c == EMPTY:
+            return count
+
+        next_pos = self.apply_dir(pos, direction)
+
+        while next_pos > -1 and self.state[next_pos] == c:
+            count += 1
+            next_pos = self.apply_dir(next_pos, direction)
+
+        return count
 
     def check_win_in_dir(self, pos: int, direction: (int, int)) -> bool:
         """
@@ -210,17 +234,34 @@ class Board:
 
         return False
 
+    def get_top_disc_positions(self) -> [int]:
+        """
+        This goes through the positions in the top row and looks down the column until it finds a non-empty position.
+        It adds that position to the array to return to get the highest disc positions in each column.
+        :return: Array of positions of the highest discs for each column.
+        """
+        starting_top_pos = (BOARD_HEIGHT - 1) * BOARD_WIDTH
+        top_disc_positions = []
+        for top_pos in range(starting_top_pos, BOARD_SIZE):
+            while top_pos > 0 and self.state[top_pos] == EMPTY:
+                top_pos -= BOARD_WIDTH
+            if top_pos > 0:
+                top_disc_positions.append(top_pos)
+
+        return top_disc_positions
+
     def who_won(self) -> int:
         """
         Check whether either side has won the game and return the winner
         :return: If one player has won, that player; otherwise EMPTY
         """
-        for start_pos in self.WIN_CHECK_DIRS:
-            if self.state[start_pos] != EMPTY:
-                for direction in self.WIN_CHECK_DIRS[start_pos]:
-                    res = self.check_win_in_dir(start_pos, direction)
-                    if res:
-                        return self.state[start_pos]
+        for top_pos in enumerate(self.get_top_disc_positions()):
+            for win_direction in self.WIN_CHECK_DIRS:
+                count_in_a_row = 0
+                for direction in win_direction:
+                    count_in_a_row += self.check_win_in_dir(top_pos, direction)
+                    if count_in_a_row >= 4:
+                        return self.state[top_pos]
 
         return EMPTY
 
@@ -229,14 +270,7 @@ class Board:
         Check whether either side has won the game
         :return: Whether a side has won the game
         """
-        for start_pos in self.WIN_CHECK_DIRS:
-            if self.state[start_pos] != EMPTY:
-                for direction in self.WIN_CHECK_DIRS[start_pos]:
-                    res = self.check_win_in_dir(start_pos, direction)
-                    if res:
-                        return True
-
-        return False
+        return self.who_won() != EMPTY
 
     def state_to_char(self, pos, html=False):
         """
@@ -250,10 +284,10 @@ class Board:
         if (self.state[pos]) == EMPTY:
             return '&ensp;' if html else ' '
 
-        if (self.state[pos]) == NAUGHT:
-            return 'o'
+        if (self.state[pos]) == BLACK:
+            return 'b'
 
-        return 'x'
+        return 'r'
 
     def html_str(self) -> str:
         """
---
+++ README.md
@@ -0,0 +1,3 @@
+# Connect Four Neural Net
+
+This application is a clone of a tutorial project, which can be found at https://github.com/fcarsten/tic-tac-toe. The original project was designed to compare different types of nueral nets for playing the game tic-tac-toe. This project builds upon one of those models to play a game of Connect Four. 
